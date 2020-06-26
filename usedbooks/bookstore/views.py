910032bb409407eaf2f247f43572189fc1d7dd21
Tanvee Balhara <balharatanvee@gmail.com>
2020-06-26 18:04:05 +0000

    Uploaded view.py

---
+++ usedbooks/bookstore/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'bookstore/home.html', {'title' : 'HOME'})
+
+def buy(request):
+    context = {
+        'title': 'BUY'
+    }
+    return render(request, 'bookstore/buy.html', context)
+
+def sell(request):
+    return render(request, 'bookstore/sell.html', {'title' : 'SELL BOOKS'})
