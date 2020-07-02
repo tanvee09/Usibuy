8c8144086c357ba01a3230885a0478260fb21c5b
Vidhi Shah <vidhi11@somaiya.edu>
2020-07-02 21:55:40 +0530

    Added CSS to all the forms

--- usedbooks/bookstore/models.py
+++ usedbooks/bookstore/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Book(models.Model):
-    title = models.CharField(max_length = 200,default = '')
-    description = models.TextField(default = '')
-    price = models.IntegerField(default = 0)
-    author = models.CharField(max_length = 200, default = '')
-    author_id = models.ForeignKey(User, on_delete = models.CASCADE,default=None, null=True, blank=True)
-    image = models.ImageField(default = 'default.jpg', upload_to = "media/profile_pics")
+    title = models.CharField(max_length=200, default='')
+    description = models.CharField(max_length=500, default='')
+    price = models.IntegerField(default=0)
+    author = models.CharField(max_length=200, default='')
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to="media/profile_pics")
 
-
-    def __str__(self) :
+    def __str__(self):
         return self.title
--- usedbooks/bookstore/urls.py
+++ usedbooks/bookstore/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
    path('', views.home, name = 'bookstore-home'),
    path('buy/', views.buy, name = 'bookstore-buy'),
    path('sell/', views.sell, name = 'bookstore-sell'),
-   path('<int:book_id>/', views.detail, name = "book-detail"),
+   path('<int:pk>/', views.detail, name = "book-detail"),
 ]
