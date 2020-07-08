a7de76fda03c98fff5c28b18762d8d96cafd15fa
Vidhi Shah <vidhi11@somaiya.edu>
2020-07-08 18:56:22 +0530

    register error solved

--- usedbooks/users/models.py
+++ usedbooks/users/models.py
@@ -3,16 +3,21 @@ from django.contrib.auth.models import User
 from PIL import Image
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    
-    def __str__(self) :
+    college = models.CharField(default='Enter college', max_length=100)
+    phone = models.BigIntegerField(default=0)
+    about = models.TextField(default='')
+
+    def __str__(self):
         return f'{self.user.username} Profile'
-    
-    def save(self, *args, **kwargs) :
+
+    def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
         img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300 :
+        if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
