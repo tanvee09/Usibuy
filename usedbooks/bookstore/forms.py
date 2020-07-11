90d28ce13d6c2708cef1b71d4588d344399880e0
Rishika Agarwal <rishika@LAPTOP-T9VEAEQR.localdomain>
2020-07-11 11:28:11 +0530

    Pdf section created

--- usedbooks/bookstore/forms.py
+++ usedbooks/bookstore/forms.py
@@ -6,3 +6,8 @@ class SellForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title' , 'description', 'price', 'author', 'image']
+
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ['topic', 'author', 'pdf']
